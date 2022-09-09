a96311bf2d514a5c331b950823492e2a5e50ffa0
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-09-09 00:21:06 +0000

    Fri, Sep 9, 2022, 12:21 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -20,7 +20,7 @@ int main(void)
             for(int column = 0; column < height; column++)
             {
                 //printf("#") ;
-                for(string hash = "#"  )
+                for(string hash = "#"; )
                 {
                     printf("%s", hash) ;
                     hash++ ;
