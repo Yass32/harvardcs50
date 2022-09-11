bd1309cc4c303b9261baf97d82d578ac9203ba25
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-09-11 14:49:39 +0000

    Sun, Sep 11, 2022, 2:49 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,28 +1,32 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void pyramid(void);
-
 int main(void)
 {
     int height, row, column;
-    char h = '#';
 
     //Prompt user for input
     do
     {
         height = get_int("Height: ") ;
     }
-    while( 1 >= height && height <= 8 ) ;
+    while( 1 >= height && height <= 8 )
 
     //For each row
     for(row = 1; row <= height; row++)
     {
         //For each column
-        for(column = 1; column <= row; column++)
+        for(column = height; column < 0; column--)
         {
             //Print brick
-            printf("°");
+            if(column > row)
+            {
+                print(".");
+            }
+            else
+            {
+                print("#");
+            }
         }
         //Move to newline
         printf("\n") ;
