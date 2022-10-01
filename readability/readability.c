46bdd24f30b5aee869a8d8677747ba2e474ac67e
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-10-01 18:29:43 +0000

    Sat, Oct 1, 2022, 6:29 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -20,11 +20,15 @@ int main(void)
     int index = 0.0588 * L - 0.296 * S - 15.8 ;
 
     int number = round(index);
-        if (number > 16 || number = 16)
+        if (number < 1)
         {
             printf("Before Grade 1\n");
         }
-        else if (number < 1)
+        else if (number > 16)
+        {
+            printf("Grade 16+\n");
+        }
+        else if (number = 16)
         {
             printf("Grade 16+\n");
         }
