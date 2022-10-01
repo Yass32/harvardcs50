135212c4df80d180e590251e9811d038a7fff98d
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-10-01 17:37:40 +0000

    Sat, Oct 1, 2022, 5:37 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -9,16 +9,14 @@ int count_sentences(string input);
 
 int main(void)
 {
+    int L = (float)count_letters(input) / count_words(input) * 100 ;
+    int S = (float)count_sentences(input) / count_words(input) * 100 ;
+    int index = 0.0588 * L - 0.296 * S - 15.8 ;
+
     string input = get_string("Text: ");
     printf("%s\n", input);
-    printf("%i letters\n", round(index));
-    printf("%i words\n", count_words(input));
-    printf("%i sentences\n", count_sentences(input));
+    printf("Grade %i \n", round(index));
 
-    int L = (float)count_letters(input) / count_words(input) * 100
-    int S = (float)
-
-    int index = 0.0588 * L - 0.296 * S - 15.8
 }
 
     int count_letters(string input)
