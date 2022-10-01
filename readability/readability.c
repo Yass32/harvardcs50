98b57243ddcc624309ecc3e46570e1e524f42599
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-10-01 12:29:58 +0000

    Sat, Oct 1, 2022, 12:29 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -15,7 +15,7 @@ int main(void)
     //printf("%i words\n", count_words);
     //printf("%i sentences\n", count_sentences);
 
-    int count_letters(string text[])
+    int count_letters(string text)
     {
         for (int h = 0; h < strlen(text); h++)
         {
