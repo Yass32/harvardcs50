822163b4765a603154e68c5b5be588e60d66a964
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-10-01 18:20:45 +0000

    Sat, Oct 1, 2022, 6:20 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -24,7 +24,7 @@ int main(void)
         {
             printf("Before Grade 1\n");
         }
-        else if (number >= 16)
+        else if (number > 16 || number = 16)
         {
             printf("Grade 16+\n");
         }
