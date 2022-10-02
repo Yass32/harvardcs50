0c3513ef3b696d8db257b7ba9a84c092cb869d36
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-10-02 16:38:19 +0000

    Sun, Oct 2, 2022, 4:38 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -12,7 +12,7 @@ int main(int argc, string argv[])
 {
     if (argc == 2)
     {
-        string argv = atoi(argv[1])
+        string argv = atoi(argv[])
         if (only_digits(argv[1]) == true)
         {
             return 0;
