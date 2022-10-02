f8dd4c5955c3cba949d6c1badefa7be63f636601
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-10-02 10:39:28 +0000

    Sun, Oct 2, 2022, 10:39 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -15,59 +15,63 @@ int main(void)
     int L = (float) count_letters(input) / count_words(input) * 100 ;
     int S = (float) count_sentences(input) / count_words(input) * 100 ;
     float index = 0.0588 * L - 0.296 * S - 15.8 ;
+    
 
 
     int number = round(index);
-        if (number < 1)
-        {
-            printf("Before Grade 1\n");
-        }
-        else if (number > 16 || number == 16)
-        {
-            printf("Grade 16+\n");
-        }
-        else
-        {
-            printf("Grade %i\n", number);
-        }
+    if (number < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (number > 16 || number == 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", number);
+    }
 
 }
 
-    int count_letters(string input)
+int count_letters(string input)
+{
+    int nu = 0;
+    for (int h = 0; h <= strlen(input); h++)
     {
-        int nu = 0;
-        for (int h = 0; h <= strlen(input); h++)
+        if (isalpha(input[h]))
         {
-            if (isalpha(input[h]))
-            {
-                nu++;
-            }
+            nu++;
         }
-        return nu;
     }
+    return nu;
+}
+//Function to count number of letters by incrementing everytime alphabet is detected
 
-    int count_words(string input)
+int count_words(string input)
+{
+    int num = 1;
+    for (int i = 0; i <= strlen(input); i++)
     {
-        int num = 1;
-        for (int i = 0; i <= strlen(input); i++)
+        if (isspace(input[i]))
         {
-            if (isspace(input[i]))
-            {
-                num++;
-            }
+            num++;
         }
-        return num;
     }
+    return num;
+}
+//Function to count number of words by incrementing everytime space is detected
 
-    int count_sentences(string input)
+int count_sentences(string input)
+{
+    int j, numb = 0;
+    for (j = 0; j <= strlen(input); j++)
     {
-        int j, numb = 0;
-        for (j = 0; j <= strlen(input); j++)
+        if (input[j] == '.' || input[j] == '?' || input[j] == '!')
         {
-            if (input[j] == '.' || input[j] == '?' || input[j] == '!')
-            {
-                numb++;
-            }
+            numb++;
         }
-        return numb;
     }
+    return numb;
+}
+//Function to count number of sentences by incrementing everytime period exclamation or question mark is detected
