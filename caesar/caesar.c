5e42f05777b4e42c05940db8da06c25672da4171
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-10-11 22:02:14 +0000

    Tue, Oct 11, 2022, 10:02 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -50,7 +50,7 @@ bool only_digits(string s)
 {
     for (int i = 0; i < strlen(s); i++)
     {
-        if(isdigit(s[i]))
+        if (isdigit(s[i]))
         {
             return true;
         }
@@ -64,15 +64,15 @@ bool only_digits(string s)
 
 char rotate(char c, int k)
 {
-    if(isalpha(c))
+    if (isalpha(c))
     {
-        if(isupper(c))
+        if (isupper(c))
         {
             int p = (int) c - 65;
             int ans = (p + k) % 26;
             return ans + 65;
         }
-        else if(islower(c))
+        else if (islower(c))
         {
             int p = (int) c - 97;
             int ans = (p + k) % 26;
