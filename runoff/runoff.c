fc57f9136d23b18446a48c3a52adea0ad2c8b494
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-10-28 15:38:36 +0000

    Fri, Oct 28, 2022, 3:38 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -136,12 +136,8 @@ bool vote(int voter, int rank, string name)
             preferences[voter][rank] = i;
             return true;
         }
-        else
-        {
-            return false;
-        }
     }
-    //return false;
+    return false;
 }
 
 // Tabulate votes for non-eliminated candidates
