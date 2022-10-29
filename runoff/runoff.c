149a8aa8c8f41a65ad7de57bfc75cd698c917efb
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-10-29 09:12:18 +0000

    Sat, Oct 29, 2022, 9:12 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (int strcmp(name, candidates[i].name) == 0)
+        if (strcmp(name, candidates[i].name) == 0)
         {
             preferences[voter][rank] = i;
             return true;
