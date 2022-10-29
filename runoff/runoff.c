8c4f45bfe336efb9bd7e83b56fef814d2c6e97af
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-10-29 09:13:25 +0000

    Sat, Oct 29, 2022, 9:13 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -146,7 +146,7 @@ void tabulate(void)
     // TODO
     for (int i = 0; i < voter_count; i++)
     {
-        if (vote() == true)
+        if (vote(voter, rank, name) == true)
         {
             candidates[i].votes += 1;
         }
