d2f91042d9d99aa8c0b6eb5e01bf994dccc2d153
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-10-31 08:16:39 +0000

    Mon, Oct 31, 2022, 8:16 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -177,7 +177,7 @@ bool print_winner(void)
 int find_min(void)
 {
     // TODO
-    int min_vote = candidates[0].votes;
+    int min_vote = 0;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated == false && candidates[i].votes < min_vote)
