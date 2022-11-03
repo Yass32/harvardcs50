24a51f3007c7d5b3a084a715b8d8c1b5081ed1b3
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-11-03 09:58:11 +0000

    Thu, Nov 3, 2022, 9:58 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -194,17 +194,17 @@ bool is_tie(int min)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated && candidates[i].votes != min)
+        if (!candidates[i].eliminated && candidates[i].votes == min)
         {
-            return false; //return 0;
+            return true;
         }
-        /*else
+        else
         {
             return false;
-        }*/
-    //return false;
+            break;
+        }
     }
-    return true;
+    return false;
 }
 
 // Eliminate the candidate (or candidates) in last place
