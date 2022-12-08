edc2e8470cbd1a0214f7069dc4e84bedf42bd848
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-12-08 20:10:41 +0000

    Thu, Dec 8, 2022, 8:10 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,6 +34,7 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    int temp = 
 
     // TODO: Read samples from input file and write updated data to output file
 
