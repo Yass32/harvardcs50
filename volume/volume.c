2ad31d260c4b325669ed6fe4dbd3db0d2b256ad8
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-12-09 17:41:01 +0000

    Fri, Dec 9, 2022, 5:41 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,6 +40,11 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
+    while(fread(&buffer, sizeof(int16_t), 1, input);)
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
 
     // Close files
     fclose(input);
