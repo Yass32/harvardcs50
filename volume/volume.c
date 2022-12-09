738dae0f6d785824844bf8d8c0d1bfdb8fdbe47d
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-12-09 19:40:42 +0000

    Fri, Dec 9, 2022, 7:40 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,8 +35,8 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
-    fread(&header, sizeof(HEADER_SIZE), 1, input);
-    fwrite(&header, sizeof(HEADER_SIZE), 1, output);
+    fread(&header, HEADER_SIZE, 1, input);
+    fwrite(&header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
