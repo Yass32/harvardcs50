6abb34fe5e7400482c719490dfa22ff0e00e1cf9
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-12-09 21:56:46 +0000

    Fri, Dec 9, 2022, 9:56 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -25,6 +25,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j <= width; j++) //Loops through column
         {
+            sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue
+            sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue
+            sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue
             double Avg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0; //Find the avg color value of pixel and store in x
             int x = (int) round(Avg);
             image[i][j].rgbtBlue = x; //Applies x to each colour to create grayscale
