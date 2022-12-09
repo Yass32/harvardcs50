284754713bdd725ea81ac6056232948e91f188d3
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-12-09 21:21:53 +0000

    Fri, Dec 9, 2022, 9:21 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,9 +7,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int x = round()
-            image[i][j].rgbtBlue = 0x00;
-            image[i][j].rgbtGreen = 0x00;
+            int x = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
+            dd
         }
     }
     return;
