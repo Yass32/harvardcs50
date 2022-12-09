8dbd2ae160c2cb04ffa463d1d0a60492cdb348c7
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-12-09 21:13:30 +0000

    Fri, Dec 9, 2022, 9:13 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = 0x00;
+            image[i][j].rgbtGreen = 0x00;
+        }
+    }
     return;
 }
 
