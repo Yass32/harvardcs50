d7bac6886b4b848cb2939923e6d64351c670d443
Yass32 <108995581+Yass32@users.noreply.github.com>
2022-12-10 09:03:41 +0000

    Sat, Dec 10, 2022, 9:03 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -25,6 +25,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j <= width; j++) //Loops through column
         {
+            // Apply formula to pixel
             sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue
             sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue
             sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue
