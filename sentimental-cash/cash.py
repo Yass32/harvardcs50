da3dcac98ec4400c45191dadbccc8bac5a602549
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-01-06 20:06:40 +0000

    Fri, Jan 6, 2023, 8:06 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,9 +1,9 @@
 # TODO
 from cs50 import get_float
 
-#Ask how many coins the customer is owed
+# Ask how many coins the customer is owed
 while True:
-    change_dollars = get_float("Enter change owed ")
+    change_dollars = get_float("Enter change owed is ")
     if dollars < 0:
         print("Error, invalid amount")
         continue
@@ -13,25 +13,25 @@ while True:
 
 coins = 0
 
-#Calculate the number of quarters to give the customer
+# Calculate the number of quarters to give the customer
 while cents >= 25:
     cents = cents - 25
     coins = coins + 1
 
-#Calculate the number of dimes to give the customer
+# Calculate the number of dimes to give the customer
 while cents >= 10:
     cents = cents - 10
     coins = coins + 1
 
-#Calculate the number of nickels to give the customer
+# Calculate the number of nickels to give the customer
 while cents >= 5:
     cents = cents - 5
     coins = coins + 1
 
-#Calculate the number of pennies to give the customer
+# Calculate the number of pennies to give the customer
 while cents >= 1:
     cents = cents - 1
     coins = coins + 1
 
-#Print total number of coins to give the customer
+# Print total number of coins to give the customer
 print(coins)
