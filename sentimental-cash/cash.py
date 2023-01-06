002e6d7bbe9fd4be14790ad51242307808301cab
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-01-06 18:24:39 +0000

    Fri, Jan 6, 2023, 6:24 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,3 +1,9 @@
 # TODO
-import get_float from cs50
-change = get_float("Enter change owed ")
+import cs50
+while True:
+    change = get_float("Enter change owed ")
+    if change < 0:
+        print("Error, invalid amount")
+        continue
+    else:
+        break
