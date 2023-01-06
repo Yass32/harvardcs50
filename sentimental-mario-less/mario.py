27c6aacb5ba40c9d0425f7a9f2fc32b85e75fb33
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-01-06 13:55:38 +0000

    Fri, Jan 6, 2023, 1:55 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -8,32 +8,4 @@ while True:
         continue
 
 for i in range(height):
-    print(" " * (height - 1 - i), "#" * (i + 1), end="#")
-
-
-
-
-
-"""
-for x in range (height):
-    print((height-x - 1)*' ' + (x+1)*'#')
-
-for i in range(height):
-    for j in range(i + 1):
-        print(" " * space, "#" * i)
-        space = space - 1
-
-
-i = 0
-
-while i < height:
-    j = i
-    while j <= i:
-        print("#")
-        j = j + 1
-    i = i + 1
-
-for i in range(height):
-    for j in range(i + 1):
-        print(" " * space, "#" * i)
-"""
+    print(" " * (height - 1 - i) + "#" * (i + 1))
