3ccd94e0e39ea8b8705a592dc6869fd142316af6
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-01-06 14:31:26 +0000

    Fri, Jan 6, 2023, 2:31 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,11 +1,14 @@
 # TODO
 while True:
+    # Check if user input is a positive integer no greater than 8
     height = int(input("Enter half pyramid height: "))
-    if (1 <= height <= 8) :
+    if (1 <= height <= 8):
         break
     else:
+        # Reprompt if user gives wrong input
         print("Error, positive integer between 1 and 8, inclusive.")
         continue
 
+# Print out the pyramid
 for i in range(height):
     print(" " * (height - 1 - i) + "#" * (i + 1))
