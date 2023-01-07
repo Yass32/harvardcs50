13a2f6f3633f010aa1a19882376edca5e05f2b7b
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-01-07 18:28:46 +0000

    Sat, Jan 7, 2023, 6:28 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,3 +1,50 @@
 # TODO
-text = input("Text: ")
+# Function to count number of letters by incrementing
+# everytime alphabet is detected
+def count_letters(input):
+    nu = 0
+    for i in range(len(input)):
+        if input[i].isalpha():
+            nu = nu + 1
+    return nu
 
+
+# Function to count number of words by incrementing
+# everytime space is detected
+def count_words(input):
+    num = 1
+    for i in range(len(input)):
+        if input[i].isspace():
+            num = num + 1
+    return num
+
+
+# Function to count number of sentences by incrementing everytime
+# period exclamation or question mark is detected
+def count_sentences(input):
+    numb = 0
+    for i in range(len(input)):
+        if input[i] == "." or input[i] == "?" or input[i] == "!":
+            numb = numb + 1
+    return numb
+
+
+# Requesting input from the user
+input = input("Text: ")
+
+# Coleman-Liau index to calculate readability of a text.
+L = float(count_letters(input) / count_words(input) * 100)
+S = float(count_sentences(input) / count_words(input) * 100)
+index = 0.0588 * L - 0.296 * S - 15.8
+
+
+# Using round to approximate index to the nearest int.
+number = round(index)
+
+# Conditions depending on the value of the rounded index.
+if number < 1:
+    print("Before Grade 1")
+elif number > 16 or number == 16:
+    print("Grade 16+")
+else:
+    print(f"Grade {number}")
