7860b87d61fcc7a480a023c55065c4ec7f1c6ef7
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-01-09 19:44:35 +0000

    Mon, Jan 9, 2023, 7:44 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -37,8 +37,8 @@ def main():
     #profile["AGATC","TTTTTTCT","AATG","TCTAG","GATA","TATC","GAAA","TCTG"] = [AAGATC_count, TTTTTTCT_count, AATG_count, TCTAG_count, GATA_count, TATC_count, GAAA_count, TCTG_count]
 
     # TODO: Check database for matching profiles
-    for i in csv_reader:
-        if csv_reader[i] == profile
+    """for i in csv_reader:
+        if csv_reader[i] == profile:"""
 
     return
 
