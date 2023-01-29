b0fb8fd6ca572832800a9eafc46182f333dec2c2
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-01-29 14:50:47 +0000

    Sun, Jan 29, 2023, 5:50 PM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -50,8 +50,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        new_person->parents[0] = parent0;
-        new_person->parents[1] = parent1;
+        new_person->parents[0] = *parent0;
+        new_person->parents[1] = *parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
         new_person->alleles[0] = new_person->parents[0]->alleles[rand() % 2];
