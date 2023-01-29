0b3606a2bee6b7ad27667be35327ce253aad2223
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-01-29 15:05:24 +0000

    Sun, Jan 29, 2023, 6:05 PM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -84,6 +84,10 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
+    if (p == NULL)
+    {
+        return;
+    }
 
     // TODO: Free parents recursively
 
