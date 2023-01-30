fd41abb40d7a244866974c96c7c1cfe2e8e533cc
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-01-30 19:44:52 +0000

    Mon, Jan 30, 2023, 10:44 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -36,9 +36,9 @@ bool load(const char *dictionary)
     {
     }*/
 
-    while (fscanf(dict_file, "%s", buffer) != 0)
+    while (fscanf(dict_file, "%s", buffer) != 0) //Read strings from file one at a time
     {
-        node *new_node = malloc(sizeof(node));
+        node *new_node = malloc(sizeof(node)); //
 
         strcpy(word_node->word, buffer);
         word_node->next = table[hash(buffer)];
