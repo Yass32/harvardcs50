510fe136c5c322fb48e82f9635894f5e1eb8bf3d
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-01-31 14:14:46 +0000

    Tue, Jan 31, 2023, 5:14 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 #include <ctype.h>
 #include <stdbool.h>
@@ -23,25 +24,37 @@ const unsigned int N = 26;
 node *table[N];
 
 /*Allocate memory for a new node
-node *new_node = malloc(sizeof(node));*/
+node *n = malloc(sizeof(node));*/
+
+int words_count = 0; //To find the number of words in dictionary using load function
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
     // TODO
     FILE *dict_file = fopen(dictionary, "r"); //Open dictionary file
-    if (file == NULL)
+    if (dict_file == NULL)
     {
         return false;
     }
 
+    char word[LENGTH + 1];
+
     while (fscanf(dict_file, "%s", word) != EOF) //Read strings from file one at a time
     {
         node *new_node = malloc(sizeof(node)); //Create a new node for each word
+        if (new_node == NULL)
+        {
+            return false;
+        }
+        strcpy(new_node->word, word);
 
-        strcpy(word_node->word, buffer);
-        word_node->next = table[hash(buffer)];
-        table[hash(buffer)] = word_node;
+        int hash_value = hash(word); //Hash word to obtain a hash value
+
+        new_node->next = table[hash_value]; //Insert node into hash table at that location
+        table[hash_value] = new_node;
+
+        words_count++; //For size function
     }
 
     fclose(dict_file);
@@ -52,20 +65,44 @@ bool load(const char *dictionary)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    //return toupper(word[0]) - 'A';
+    int value = strlen(word) % N; //Using hash function example3; Math using all letters
+    if (value > N)
+    {
+        return value % N;
+    }
+    else
+    {
+        return value;
+    }
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return words_count;
 }
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
     // TODO
+    int hash_value = hash(word); //Hash word to obtain a hash value
+
+    node *cursor = table[hash_value]; //Access linked list at that index in the hash table
+
+    while (cursor != NULL) //Traverse linked list, looking for the word
+    {
+        if(strcasecmp(word, cursor->word) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            cursor = cursor->next;
+        }
+    }
     return false;
 }
 
@@ -73,6 +110,36 @@ bool check(const char *word)
 bool unload(void)
 {
     // TODO
+    for (int i = 1; i <= N; i++)
+    {
+        if (table[i] != NULL)
+        {
+            node *cursor = table[i];
+            while (cursor != NULL)
+            {
+                node *tmp = cursor;
+                cursor = cursor->next;
+                free(tmp);
+            }
+        }
+        return true;
+
+        /*while (cursor != NULL)
+        {
+            node *tmp = cursor;
+            cursor = cursor->next;
+            free(tmp);
+        }
+
+        if (tmp == NULL && cursor == NULL)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }*/
+    }
     return false;
 }
 
