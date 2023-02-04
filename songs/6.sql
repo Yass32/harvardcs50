e778805a4447ffad263e886aef9979268e8b3c47
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-04 18:53:43 +0000

    Sat, Feb 4, 2023, 9:53 PM +03:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,2 +1,2 @@
 --write a SQL query that lists the names of songs that are by Post Malone
-SELECT name from songs WHERE id IN (SELECT id  FROM artists WHERE name = 'Post Malone');
+SELECT name from songs WHERE artist_id IN (SELECT id  FROM artists WHERE name = 'Post Malone');
--- songs/7.sql
+++ songs/7.sql
@@ -1,2 +1,2 @@
 --write a SQL query that returns the average energy of songs that are by Drake
-SELECT AVG(energy) from songs WHERE id IN (SELECT id  FROM artists WHERE name = 'Drake');
+SELECT AVG(energy) from songs WHERE artist_id IN (SELECT id  FROM artists WHERE name = 'Drake');
