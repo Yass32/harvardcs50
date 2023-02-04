4e5ec13a70eda0bf61e29f495ceee4ab3862fd35
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-04 18:29:24 +0000

    Sat, Feb 4, 2023, 9:29 PM +03:00

--- songs/3.sql
+++ songs/3.sql
@@ -1,2 +1,2 @@
 --write a SQL query to list the names of the top 5 longest songs, in descending order of length
-SELECT name FROM songs ORDER BY duration_ms DESC;
+SELECT name FROM songs ORDER BY duration_ms DESC LIMIT 5;
