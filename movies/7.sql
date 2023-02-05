73561e26c012c91aa50ab8a0641a3d18777f8bdc
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 11:17:43 +0000

    Sun, Feb 5, 2023, 2:17 PM +03:00

--- movies/7.sql
+++ movies/7.sql
@@ -2,7 +2,7 @@
 --For movies with the same rating, order them alphabetically by title
 
 (SELECT title FROM movies WHERE year = 2010
-UNION
+UNION ALL
 SELECT rating FROM ratings WHERE movie_id IN (SELECT id FROM movies WHERE year = 2010)) ORDER BY rating DESC, title ;
 
 --(SELECT title AS titles1 FROM movies WHERE year = 2010)
