6777bdf927228261fd5abd1106ab9597398f8fc4
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 11:40:51 +0000

    Sun, Feb 5, 2023, 2:40 PM +03:00

--- movies/7.sql
+++ movies/7.sql
@@ -4,7 +4,8 @@
 SELECT title AS t FROM movies WHERE year = 2010
 UNION ALL
 SELECT rating AS r FROM ratings WHERE movie_id IN (SELECT id FROM movies WHERE year = 2010)
-ORDER BY rating t, r ;
+ORDER BY rating t, r
+ ;
 
 --(SELECT title AS titles1 FROM movies WHERE year = 2010)
 --UNION
