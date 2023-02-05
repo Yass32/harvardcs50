26dad164e572a4d72e6c3e49bcc14d056c6bed0c
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 20:09:16 +0000

    Sun, Feb 5, 2023, 11:09 PM +03:00

--- movies/9.sql
+++ movies/9.sql
@@ -3,8 +3,4 @@ SELECT name FROM people
     WHERE id IN (
     SELECT person_id FROM stars
     WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004)
-    );
-
-
-
-SELECT id FROM movies WHERE year = 2004
+    ) ORDER BY birth;
