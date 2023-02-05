3a8f3032192af29bceb783528a9f63895a9cef05
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 19:56:54 +0000

    Sun, Feb 5, 2023, 10:56 PM +03:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,2 +1,6 @@
 --write a SQL query to list the names of all people who starred in Toy Story
+SELECT person_id FROM stars WHERE movie_id IN ()
+
 SELECT id FROM movies WHERE title = 'Toy Story'
+
+SELECT name from songs WHERE artist_id IN (SELECT id  FROM artists WHERE name = 'Post Malone');
