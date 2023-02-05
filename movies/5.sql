538a276704edbeb3eee0cd1bb7eca9653d9cbd46
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 08:55:18 +0000

    Sun, Feb 5, 2023, 11:55 AM +03:00

--- movies/5.sql
+++ movies/5.sql
@@ -1,2 +1,4 @@
 --write a SQL query to list the titles and release years of all Harry Potter movies, in chronological order
-SELECT title, year FROM movies WHERE title LIKE 'Harry Potter%' ORDER BY year;
+SELECT title, year FROM movies
+   ...> WHERE title LIKE 'Harry Potter%'
+   ...> ORDER BY year;
---
+++ movies/6.sql
@@ -0,0 +1,1 @@
+--write a SQL query to determine the average rating of all movies released in 2012
