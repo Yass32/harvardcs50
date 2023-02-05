05bba4f16286eae68af1dec4d26133d470b00db2
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 22:48:54 +0000

    Mon, Feb 6, 2023, 1:48 AM +03:00

--- movies/11.sql
+++ movies/11.sql
@@ -2,10 +2,8 @@
 --that Chadwick Boseman starred in, starting with the highest rated.
 
 SELECT title FROM movies
+    JOIN ratings ON movies.id = ratings.movie_id
     WHERE id IN (
-    SELECT movie_id FROM ratings
-    WHERE movie_id = (
     SELECT movie_id FROM stars
     WHERE person_id IN (SELECT id FROM people WHERE name = 'Chadwick Boseman')
-    ) ORDER BY rating DESC LIMIT 5
-    ) ;
+    ) ORDER BY rating DESC LIMIT 5;
