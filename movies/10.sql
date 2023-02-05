f0a3cc095cbd072f6960e0fbb74ac79f240d866f
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 20:21:21 +0000

    Sun, Feb 5, 2023, 11:21 PM +03:00

--- movies/10.sql
+++ movies/10.sql
@@ -4,5 +4,5 @@ SELECT name FROM people
     SELECT person_id FROM directors
     WHERE movie_id IN (
     SELECT movie_id FROM ratings
-    WHERE rating IN (SELECT id FROM movies) >= 9.0
+    WHERE movie_id IN (SELECT id FROM movies) AND rating >= 9.0
     ));
