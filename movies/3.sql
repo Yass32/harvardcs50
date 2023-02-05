246d6491fdbf900ba0fd8e9c38f3b8ae771addbe
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 08:44:19 +0000

    Sun, Feb 5, 2023, 11:44 AM +03:00

--- movies/3.sql
+++ movies/3.sql
@@ -1,1 +1,4 @@
 --write a SQL query to list the titles of all movies with a release date on or after 2018, in alphabetical order
+SELECT title FROM movies
+   ...> Where year >= 2018
+   ...> ORDER BY title ASC;
