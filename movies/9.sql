21da8a45b58da21566ae52d9fac37e0eb59c0a64
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 20:08:14 +0000

    Sun, Feb 5, 2023, 11:08 PM +03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,2 +1,10 @@
 --write a SQL query to list the names of all people who starred in a movie released in 2004, ordered by birth year
-SELECT id FROM movies WHERE 
+SELECT name FROM people
+    WHERE id IN (
+    SELECT person_id FROM stars
+    WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004)
+    );
+
+
+
+SELECT id FROM movies WHERE year = 2004
