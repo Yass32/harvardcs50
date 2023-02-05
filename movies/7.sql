9782112e6024823f1569e4f8ac48f23e69b5c464
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 09:03:36 +0000

    Sun, Feb 5, 2023, 12:03 PM +03:00

---
+++ movies/7.sql
@@ -0,0 +1,3 @@
+--write a SQL query to list all movies released in 2010 and their ratings, in descending order by rating.
+--For movies with the same rating, order them alphabetically by title
+SELECT title, FROM movies
