a980848124529a1c1ff8161ce4a2437f028a6f2f
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 20:47:20 +0000

    Sun, Feb 5, 2023, 11:47 PM +03:00

--- movies/11.sql
+++ movies/11.sql
@@ -3,7 +3,7 @@
 
 SELECT title FROM movies
     WHERE id IN (
-    SELECT person_id FROM stars
+    SELECT movie_id FROM stars
     WHERE movie_id IN (
     SELECT movie_id FROM ratings
     WHERE person_id IN (SELECT id FROM people WHERE name = 'Chadwick Boseman') ORDER BY rating LIMIT 5
