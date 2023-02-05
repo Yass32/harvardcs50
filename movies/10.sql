abde5b493dcb9c42b3034162c043f4ac5734d008
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-05 20:17:48 +0000

    Sun, Feb 5, 2023, 11:17 PM +03:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,6 +1,8 @@
 --write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0
 SELECT name FROM people
     WHERE id IN (
+    SELECT person_id FROM diractors
+    WHERE movie_id IN (
     SELECT movie_id FROM ratings
     WHERE rating IN (SELECT id FROM movies) >= 9.0
-    );
+    ));
