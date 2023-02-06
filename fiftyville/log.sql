f0c5dcc41e806b93cfb63083dde6760159f8dc81
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-06 18:21:29 +0000

    Mon, Feb 6, 2023, 9:21 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,4 +8,6 @@ SELECT description FROM crime_scene_reports WHERE day = 28 AND month = 7 AND str
 --present at the time – each of their interview transcripts mentions the bakery.
 
 --Ceck interview transcript of witnesses
+--According Rto Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
+--If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.
 
