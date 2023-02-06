58bc292afad48add5d3e5ac06cc8aa096f697333
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-06 15:39:00 +0000

    Mon, Feb 6, 2023, 6:39 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+--Convert database to excel to better understand the tables and their interconnections
+
+--Get Crime report for the day time and place the crime took place
