e4616aab665bbd0f17ed110c3fdf82ed5c1d30ee
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-06 15:01:07 +0000

    Mon, Feb 6, 2023, 6:01 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -7,16 +7,3 @@ SELECT name from people WHERE id IN (
         )
     )
 );
-
-SELECT name
-FROM people JOIN stars ON stars.person_id = people.id
-WHERE id IN (
-    SELECT person_id FROM stars WHERE movie_id IN(
-        SELECT movie_id FROM stars WHERE person_id IN (
-            SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = 1958
-        )
-    )
-);
-SELECT name FROM people WHERE name != "Kevin Bacon" AND id IN
-(SELECT DISTINCT person_id FROM stars WHERE movie_id IN
-(SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = "1958")));
