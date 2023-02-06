65ff925760e5ff7181f21852fd0b23a42e24d81f
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-06 14:01:35 +0000

    Mon, Feb 6, 2023, 5:01 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -2,10 +2,10 @@
 
 SELECT name FROM people WHERE id IN (
     SELECT person_id WHERE movies_id IN (
-        SELECT movies_id FROM stars WHERE people_id IN (
+        SELECT movies_id FROM stars WHERE person_id IN (
         SELECT id FROM people WHERE name = 'Kevin Bacon' )
         INTERSECT
-        SELECT movies_id FROM stars WHERE people_id IN (
+        SELECT movies_id FROM stars WHERE person_id IN (
         SELECT id FROM people )
     )
 );
