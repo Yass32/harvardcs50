26012c4ad6bf4c111a58c2fb96210f1a75d61224
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-06 14:17:45 +0000

    Mon, Feb 6, 2023, 5:17 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,11 +1,6 @@
 --write a SQL query to list the names of all people who starred in a movie in which Kevin Bacon also starred
 
-SELECT name FROM people WHERE id IN (
-    SELECT person_id FROM stars WHERE movies_id IN (
-        SELECT movies_id FROM stars WHERE person_id IN (
-        SELECT id FROM people WHERE name = 'Kevin Bacon' )
-        INTERSECT
-        SELECT movies_id FROM stars WHERE person_id IN (
-        SELECT id FROM people )
-    )
-);
+SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = 1958
+
+SELECT movies_id FROM stars WHERE person_id IN
+
