b796678e9b35c52dd294f7e3f9b00a6012efe8c0
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-06 18:15:54 +0000

    Mon, Feb 6, 2023, 9:15 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,6 +3,8 @@
 --Convert database to excel to better understand the tables and their interconnections
 
 --Get Crime report for the day time and place the crime took place
-SELECT description FROM crime_scene_reports WHERE day = 28 AND month = 7 AND street = 'Humphrey Street';
+SELECT description FROM crime_scene_reports WHERE day = 28 AND month = 7 AND street = 'Humphrey Street' LIMIT 1;
+--Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were
+--present at the time – each of their interview transcripts mentions the bakery.
 
 --
