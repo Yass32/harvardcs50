86a8e334b02bc7752d788b53d8a525a1acb7b363
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-06 22:40:40 +0000

    Tue, Feb 7, 2023, 1:40 AM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -88,6 +88,8 @@ SELECT name FROM people WHERE passport_number IN (
 );
 
 --Check for accomplice by finding who called Bruce
-SELECT receiver FROM phone_calls WHERE caller IN (
-    SELECT phone_number FROM people WHERE name = 'Bruce'
-)
+SELECT name FROM people WHERE phone_number IN (
+    SELECT receiver FROM phone_calls WHERE caller IN (
+        SELECT phone_number FROM people WHERE name = 'Bruce'
+    )
+);
