ad7ea087b2884634bf51bd4d687685601712d109
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-06 13:19:20 +0000

    Mon, Feb 6, 2023, 4:19 PM +03:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,7 +1,9 @@
 --write a SQL query to list the titles of all movies in which both Johnny Depp and Helena Bonham Carter starred
 
-SELECT title FROM movies WHERE id = (
+SELECT title FROM movies WHERE id IN (
     SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Johnny Depp')
-    INTERSECT
+)
+INTERSECT
+SELECT title FROM movies WHERE id IN (
     SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Helena Bonham Carter')
-    );
+);
