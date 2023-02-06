700bc23bc3913603fb78e4702c350288de2c5592
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-06 12:09:49 +0000

    Mon, Feb 6, 2023, 3:09 PM +03:00

--- movies/12.sql
+++ movies/12.sql
@@ -3,7 +3,7 @@
 SELECT title FROM movies WHERE id IN (
     SELECT movie_id FROM stars WHERE person_id = (
         SELECT id FROM people WHERE name = 'Johnny Depp'
-        UNION
+        UNION ALL
         SELECT id FROM people WHERE name = 'Helena Bonham Carter'
     )
 );
