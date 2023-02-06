9c8d39babd822afbdb6ec9c2543c93b91df72962
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-02-06 18:38:17 +0000

    Mon, Feb 6, 2023, 9:38 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -13,3 +13,5 @@ SELECT transcript FROM interviews WHERE day = 28 AND month = 7;
 --According Ruth(witness 1) to: Sometime within ten minutes of the theft (10:15am), I saw the thief get into a car in the bakery parking lot and drive away.
 --If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.
 
+Check bakery security log for cars exiting within
+
