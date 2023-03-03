fc69056f8d975d4ee633f1253b4146ae65ae7ec5
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-03-03 14:38:02 +0000

    Fri, Mar 3, 2023, 5:38 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -12,6 +12,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///birthdays.db")
 
+entries = db.execute("SELECT * FROM ")
+
 
 @app.after_request
 def after_request(response):
