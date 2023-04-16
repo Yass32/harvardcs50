2fed97d84206c8d383c3ef84239ccb1c458fc2e6
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-04-16 02:46:46 +0000

    Sun, Apr 16, 2023, 5:46 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -130,6 +130,8 @@ def register():
     ##When requested via POST, check for errors
         if not username or username in users:
             return apology("Username Error")
+        if username in users:
+            return apology("Username Taken")
         if not password or password != confirmation or password in users:
             return apology("Password Error")
 
