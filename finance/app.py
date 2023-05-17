86822c1c90723a8cb70475fe454755763e67e229
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-05-17 16:35:13 +0000

    Wed, May 17, 2023, 7:35 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -42,10 +42,7 @@ def index():
     """Show portfolio of stocks"""
     current_user = session["user_id"]
     cash = db.execute("SELECT cash FROM users WHERE id = ?", current_user)
-    stock = db.execute("SELECT stocks FROM portfolio WHERE username_id = ?", current_user)
-    shares = db.execute("SELECT shares FROM portfolio WHERE username_id = ?", current_user)
-    price = db.execute("SELECT price FROM portfolio WHERE username_id = ?", current_user)
-    portfolio = db.execute("SELECT stocks, shares, price, total FROM portfolio GROUP BY shares HAVING username_id = ?", current_user)
+    portfolio = db.execute("SELECT stocks, shares, price, total FROM portfolio GROUP BY stock HAVING username_id = ?", current_user)
     balance = portfolio["total"] * cash
     return render_template("index.html", stock = portfolio["stock"], shares = portfolio["shares"], price = portfolio["price"], total = portfolio["total"], balance = balance)
 
