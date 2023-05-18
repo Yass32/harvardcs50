1803209fbdfbafb0597a3c4fd318cdd82be2ff71
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-05-18 10:20:33 +0000

    Thu, May 18, 2023, 1:20 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -77,15 +77,16 @@ def buy():
             return apology("Insufficient cash")
         else:
             updated_cash = user_cash - price
-            db.execute("UPDATE users SET cash = ? WHERE id = ?", (updated_cash, current_user))
+            db.execute("UPDATE users SET cash = ? WHERE id = ?", updated_cash, current_user)
             db.execute("INSERT INTO portfolio (symbol, stocks, shares, price, total, username_id) VALUES (?, ?, ?, ?, ? )", symbol, stocks, shares, price, total, current_user)
+            return redirect("/")
 
 
 
 
-        return redirect("/")
 
-    return apology("TODO")
+
+        return apology("TODO")
 '''
 CREATE TABLE portfolio (
     username_id INTEGER NOT NULL,
