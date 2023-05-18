740f592b1b5460b4b20daafce63d9da55609b1df
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-05-18 13:00:28 +0000

    Thu, May 18, 2023, 4:00 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -46,7 +46,7 @@ def index():
     portfolio = db.execute("SELECT symbol, stocks, price, total, SUM(shares) as total_shares FROM portfolio WHERE username_id = ? GROUP BY symbol", current_user)
     balance = user_cash
     for port in portfolio:
-        balance += portfolio["total_shares"] * portfolio["price"]
+        balance += portfolio["price"]
 
     return render_template("index.html", portfolio = portfolio, cash = user_cash, balance = balance)
 
