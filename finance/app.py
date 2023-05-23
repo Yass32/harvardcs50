3fe50a9607ce414c3a3b52b6f2c2596a97de80e7
Yass32 <108995581+Yass32@users.noreply.github.com>
2023-05-23 08:55:55 +0000

    Tue, May 23, 2023, 11:55 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -315,7 +315,8 @@ def change():
 
         # To check if the hash of the old password provided by the user matches the hashed password stored in the database
         row = db.execute("SELECT * FROM users WHERE username = ?", username)
-        if not row or not check_password_hash(row[0]["hash"], generate_password_hash(old_password)):
+        old_password = generate_password_hash(old_password)
+        if not row or not check_password_hash(row[0]["hash"], old_password):
             return apology("Username or Password is incorrect")
 
         try:
